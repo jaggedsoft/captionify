2588f77fd985ac47debfe1f5404733aa651dc111
bolatovumar <bolatovumar@gmail.com>
2015-07-25 20:17:00 -0700

    Added download link event tracking for analytics

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,35 @@
 (function () {
     'use strict';
-    var ex2 = new Captionify({
+    var ex1 = new Captionify({
         imgClassSelector: "caption"
     });
 
-    var ex3 = new Captionify({
+    var ex2 = new Captionify({
         imgClassSelector: "data-caption",
         dataCaption: true
     });
 
-    var ex6 = new Captionify({
+    var ex3 = new Captionify({
         imgClassSelector: "bottom-caption",
         mode: "bottom"
     });
+
+    var downloadLink = document.getElementById('download');
+    addListener(downloadLink, 'click', function() {
+        ga('send', 'event', 'button', 'click', 'captionify-download');
+    });
+
+    /**
+     * Utility to wrap the different behaviors between W3C-compliant browsers
+     * and IE when adding event handlers.
+     *
+     * @param {Object} element Object on which to attach the event listener.
+     * @param {string} type A string representing the event type to listen for
+     *     (e.g. load, click, etc.).
+     * @param {function()} callback The function that receives the notification.
+     */
+    function addListener(element, type, callback) {
+        if (element.addEventListener) element.addEventListener(type, callback);
+        else if (element.attachEvent) element.attachEvent('on' + type, callback);
+    }
 }());
